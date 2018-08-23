b0009717de7a7720e81a858fb32535ece29ec764
sankalpvk18 <40889688+sankalpvk18@users.noreply.github.com>
2018-08-23 19:40:58 +0530

    beautiful days at the movies program added

---
+++ Algorithms/Implementation/Beautiful days at the movies/code.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulDays function below.
+    static int beautifulDays(int i, int j, int k) {
+        
+        
+        int count = 0;
+        for(int x = i; x<=j; x++){
+            int rev = 0;
+            int num = x;
+            int temp = x;
+            while(num>0){
+                int mod = num%10;
+                rev = rev*10 + mod;
+                num = num/10;
+            }
+                
+            if(Math.abs(temp-rev) % k == 0){
+                count++;
+            }
+                
+        }
+        
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] ijk = scanner.nextLine().split(" ");
+
+        int i = Integer.parseInt(ijk[0]);
+
+        int j = Integer.parseInt(ijk[1]);
+
+        int k = Integer.parseInt(ijk[2]);
+
+        int result = beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
